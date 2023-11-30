5ccb6436eafb78a14a4e983ce14f9196024e8c78
jomarca4 <jomarca4@icloud.com>
2023-11-30 23:08:41 +0100

    static

--- myproject/settings.py
+++ myproject/settings.py
@@ -155,6 +155,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = '/var/www/myproject/static/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
