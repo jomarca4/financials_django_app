b951024a11002c337824b448783dd8c7bafd697f
jomarca4 <jomarca4@icloud.com>
2023-12-01 22:25:50 +0100

    Your commit message home page

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = financials_PRD_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 print(DEBUG)
 #print(DEBUG)
 # Fetch the environment variable
