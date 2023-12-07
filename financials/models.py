3b80d89f7bdc2a7c10d348dfab2092bfc974a42c
jomarca4 <jomarca4@icloud.com>
2023-12-07 23:16:56 +0100

    about page v2

--- financials/models.py
+++ financials/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
-
+from django.utils.text import slugify
 from django.utils import timezone
 # Create your models here.
 
